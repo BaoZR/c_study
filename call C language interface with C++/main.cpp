d4ac50c55b5d7c91e51255aa3fc3e0140817c5b6
BaoZhengRan <474234165@qq.com>
2020-07-15 15:31:20 +0800

    Add files via upload

---
+++ call C language interface with C++/main.cpp
@@ -0,0 +1,10 @@
+#include"test.h"
+#include<iostream>
+using namespace std;
+int main(void)
+{
+    cout<<"start to call c function"<<endl;
+    testCfun();
+    cout<<"end to call c function"<<endl;
+    return 0;
+}
---
+++ call C language interface with C++/test.c
@@ -0,0 +1,6 @@
+#include"test.h"
+void testCfun()
+{
+    printf("I am c fun\n");
+    return;
+}
